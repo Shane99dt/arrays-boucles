9ff9026720af6e51b600c65c1ae153c6e2a97ae2
Shane99dt <dushen1999thilaksha@gmail.com>
2022-06-22 11:30:18 +0200

    arrays exercise

---
+++ index.js
@@ -0,0 +1,46 @@
+// const fruits = ["mango", "lemon", "blueberry"]
+
+// console.log(fruits)
+// console.table(fruits)
+
+// const ingredients = ["eggs", "milk", "butter"]
+// console.log(ingredients[1])
+// console.log(ingredients.indexOf("butter"))
+
+
+// const objects = ["pen", "book", "lamp"]
+
+// objects.unshift("chair")
+// console.log(objects)
+
+// objects.pop()
+// console.log(objects)
+
+// objects.push("laptop")
+// console.log(objects)
+
+// objects.shift()
+// console.log(objects)
+
+// const numbers = [4, 10, 8, 12, 6]
+
+// numbers.reverse()
+// console.log(numbers)
+
+// let total = 0
+// const limit = 10
+
+// for( i = 1; i <= 10; i++){
+//   total += i
+// }
+// console.log(total)
+
+// const sentence = "Hello Konexio !"
+// // console.log(sentence.length)
+// let sentenceReversed = []
+
+// for(let i = sentence.length; i >= 0; i--){
+//   sentenceReversed.push(sentence[i])
+//   // console.log(sentence[i])
+// }
+// console.log(sentenceReversed.join(""))
